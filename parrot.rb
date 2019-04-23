6acfad2b967d7b225a301370b56d61bef50cae92
Gary Wheeler <gkwheeler40@gmail.com>
2019-04-23 12:50:03 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,6 +1,6 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 def parrot(express="Squawk!")
-  puts "Pretty bird!" 
+  puts "Squawk!" 
   return "Pretty bird!" 
 end
